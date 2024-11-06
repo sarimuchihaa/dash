64479c846a0fd11a005f9dead545112486ed46de
sarim <sarimslayerali786@gmail.com>
2024-11-06 13:58:55 +0500

    Commit sixth calendar done

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -2,11 +2,33 @@ import * as React from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import dayjs from 'dayjs';
+import { styled } from '@mui/material/styles';
+
+const CustomDateCalendar = styled(DateCalendar)({
+  '& .MuiDayCalendar-weekDayLabel': {
+    fontWeight: 'bold',
+    color: '#000000',
+  },
+  '& .MuiPickersDay-root.Mui-selected': {
+    backgroundColor: '#1976d2',
+    color: '#ffffff',
+    '&:hover': {
+      backgroundColor: '#115293',
+    },
+  },
+  '& .MuiIconButton-root': {
+    color: '#1976d2', 
+    '&:hover': {
+      color: '#115293',
+    },
+  },
+});
 
 export default function Calendar() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar />
+      <CustomDateCalendar defaultValue={dayjs()} />
     </LocalizationProvider>
   );
 }
