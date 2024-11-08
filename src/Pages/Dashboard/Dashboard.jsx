4020e1245c138ce780c497761a9292568b038231
sarim <sarimslayerali786@gmail.com>
2024-11-08 04:15:38 -0800

    ok

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,28 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Projects from '../../Components/Projects/Projects';
 import Activity from '../../Components/Activity/Activity';
 import MyTask from '../../Components/MyTask/MyTask';
-import Popup from '../../Components/Popup/Popup';
+
 
 export const Dashboard = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false); // Modal state
-
   return (
     <div>
       <Routes>
         <Route path="/" element={
           <>
-            {/* Conditionally render components based on modal state */}
-            {!isModalOpen && (
-              <>
-                <Projects />
-                <Activity />
-                <MyTask />
-              </>
-            )}
-            {/* Render popup when modal is open */}
-            {isModalOpen && <Popup onClose={() => setIsModalOpen(false)} />}
+            <Projects />
+            <Activity />
+            <MyTask />
           </>
         } />
       </Routes>
