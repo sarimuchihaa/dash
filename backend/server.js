9a61a44ec93bde0ecf99e9dbad4fbe0621a125a5
sarim <sarimslayerali786@gmail.com>
2024-11-11 03:26:39 -0800

    MongoDB Connected Commit 29

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
+// IMPORTING
 import express from "express";
+import dotenv from "dotenv"; 
+import dbConnection from "./db/dbConnection.js";
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
+
+
+// Route for root url ("/").
 app.get("/", (req, res) => {
-    res.send("Hello Sam");
+    res.send("Hello Sarim 😇");
 })
 
-app.listen(5000, () => console.log("Server Running on port 5000"));
+
+
+// DB Connection.
+app.listen(PORT, () => {
+    dbConnection();
+    console.log(`Server Running on port ${PORT} 😃`)
+});
